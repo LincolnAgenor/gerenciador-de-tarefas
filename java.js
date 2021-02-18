a3d8fe1b94e2086a21d83ca6efa57da180e87eeb
LincolnAgenor <lincolnagenor6@gmail.com>
2021-02-18 15:48:33 -0300

    renconstrucao-do-codigo

--- java.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#add").addEventListener("click", () =>{
-    document.querySelector(".tela1").style.display = "none";
-    document.querySelector(".tela2").style.display = "flex";
-})
-document.querySelector(".enviar").addEventListener("click", () =>{
-    document.querySelector(".tela1").style.display = "flex";
-    document.querySelector(".tela2").style.display = "none";
-})
